3f7b5c2bd2012febe770f664fae4f2da7dc3262e
Mustafa Soylemezo <soylemezom@gmail.com>
2023-06-07 08:46:41 -0400

    vs code changes

--- 1.java
+++ 1.java
@@ -5,5 +5,5 @@ int a=100,
 int b=200;
 int c=(a+b);
 System.out.println.("int sum is + c "); bugsjava codes has been checked   today  
-java codes has been updated ok no problem//// 
- main
+/////java codes has been updated ok no problem//// 
+///new changes
--- 4.java
+++
@@ -1,3 +0,0 @@
-x y z java code updates done by dev1  dev2 can also make some chnages 
-
-java codes changes
