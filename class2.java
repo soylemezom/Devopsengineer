181c079b138583a3f3bd7354a0c0e60b2712fa7e
Mustafa Soylemezo <soylemezom@gmail.com>
2023-06-10 13:07:16 -0400

    neew update

---
+++ class2.java
@@ -0,0 +1,13 @@
+public class class2 {
+
+    @Override
+    public String toString() {
+        return "class2 []";
+    }
+   
+    
+System.out.println("hello world");
+
+
+
+}
---
+++ testclasss.java
@@ -0,0 +1,25 @@
+import javax.lang.model.util.ElementScanner14;
+
+public class testclasss {
+    
+
+int x=100;
+int y=200;
+int z=(x+y);
+
+if(z=300);
+
+System.out.println("this is correct");
+
+
+
+System.out.println("this sum is wrong");
+
+
+
+
+
+
+
+
+}
